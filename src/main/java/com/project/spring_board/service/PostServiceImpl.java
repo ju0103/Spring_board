31647ea3979b9ec50cf0dc181bba488cc203f70d
yuunjuu <qqaa01035@gmail.com>
2021-11-27 18:13:08 +0900

    Feat: Add post list view function

--- src/main/java/com/project/spring_board/service/PostServiceImpl.java
+++ src/main/java/com/project/spring_board/service/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.project.spring_board.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.ibatis.session.SqlSession;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.spring_board.dao.PostDao;
+import com.project.spring_board.dto.PostDto;
 
 @Service("PostService")
 public class PostServiceImpl implements PostService {
@@ -14,10 +16,18 @@ public class PostServiceImpl implements PostService {
 	@Autowired
 	private SqlSession sqlSession;
 
+	// 게시글 작성
 	@Override
 	public void post_write(HashMap<String, String> param) {
 		PostDao dao = sqlSession.getMapper(PostDao.class);
 		dao.post_write(param);
+	}
+
+	// 게시물 목록 조회
+	@Override
+	public ArrayList<PostDto> post_list() {
+		PostDao dao = sqlSession.getMapper(PostDao.class);
+		return dao.post_list();
 	}
 	
 }
